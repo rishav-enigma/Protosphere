02abcd1821024c1ffd80360f7e904de190b4d88c
ShubhamNKhanna <68947043+ShubhamNKhanna@users.noreply.github.com>
2021-12-01 00:37:02 +0530

    Update protosphere.rb

--- lib/protosphere.rb
+++ lib/protosphere.rb
@@ -3,7 +3,6 @@
 require_relative "protosphere/version"
 
 module Protosphere
-  Dir["proto/**/*_services_pb.rb"].each {|file| require file }
   class Error < StandardError; end
   # Your code goes here...
 end
